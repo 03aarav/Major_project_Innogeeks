9ec961d4c74a73a3a1bef128e296a8aea9abbbc9
Anant Jaiswal <razorquake2499@gmail.com>
2023-08-27 16:21:14 +0530

    chat

---
+++ app/src/main/java/com/example/project_innogeeks/Model/ChatType.kt
@@ -0,0 +1,5 @@
+package com.example.project_innogeeks.Model
+
+import android.content.BroadcastReceiver
+
+data class ChatType(var senderId: String="", var receiverId: String = "", var message: String = "")
